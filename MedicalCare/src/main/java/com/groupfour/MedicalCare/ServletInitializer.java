32b6483e7fbecd5ca6bc24f64b0fa23154473392
Petar <handsomebobrrola@yahoo.com>
2019-11-01 12:21:52 +0100

    Struktura projekta

---
+++ MedicalCare/src/main/java/com/groupfour/MedicalCare/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.groupfour.MedicalCare;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MedicalCareApplication.class);
+	}
+
+}
---
+++ MedicalCare/src/test/java/com/groupfour/MedicalCare/MedicalCareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groupfour.MedicalCare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalCareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
